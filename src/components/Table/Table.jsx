d19d52ac5fbc139272c8de21c96e4bc46057d9ed
Rodrigo Andrade Monclair - NUPAC <t319871@tjdft.jus.br>
2024-03-08 15:55:15 -0300

    add pills style

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,32 +1,40 @@
-import {Tab} from 'react-bootstrap';
-import {Tabs} from 'react-bootstrap';
-import TabelaPSSS from '../Tabelas/TabelaPSSS.jsx';
-import TabelaRoo from '../Tabelas/TabelaRoo.jsx';
-import TabelaPd from '../Tabelas/TabelaPd.jsx';
-import TabelaRtj from '../Tabelas/TabelaRtj.jsx';
+import { Tab } from "react-bootstrap";
+import { Tabs } from "react-bootstrap";
+import TabelaPSSS from "../Tabelas/TabelaPSSS.jsx";
+import TabelaRoo from "../Tabelas/TabelaRoo.jsx";
+import TabelaPd from "../Tabelas/TabelaPd.jsx";
+import TabelaRtj from "../Tabelas/TabelaRtj.jsx";
 
 function Tables() {
   return (
-    <div style={{width: "80%", margin: "0px auto  0px auto", color: "#e7e7e7"}}>
-    <Tabs
-      defaultActiveKey="profile"
-      id="fill-tab-example"
-      className="mb-3"
-      fill
+    <div
+       style={{ width: "80%", margin: "0px auto  0px auto"}}
     >
-      <Tab eventKey="home" title="Ressarcimento ao TJDFT">
-        <TabelaRtj />
-      </Tab>
-      <Tab eventKey="controle de Parc. de Débito" title="Controle de Parc. de Débito">
-        <TabelaPd />
-      </Tab>
-      <Tab eventKey="profile" title="Ressarcimento a Outros Órgãos">
-        <TabelaRoo />
-      </Tab>
-      <Tab eventKey="longer-tab" title="PSSS">
-        <TabelaPSSS />
-      </Tab>
-    </Tabs>
+      <Tabs
+        defaultActiveKey="profile"
+
+        id="fill-tab-example"
+        className="mb-3"
+        variant="pills"
+        justify
+      >
+        <Tab tabClassName="test" eventKey="home" title="Ressarcimento ao TJDFT">
+          <TabelaRtj />
+        </Tab>
+        <Tab
+        tabClassName="test"
+          eventKey="controle de Parc. de Débito"
+          title="Controle de Parc. de Débito"
+        >
+          <TabelaPd />
+        </Tab>
+        <Tab tabClassName="test" eventKey="profile" title="Ressarcimento a Outros Órgãos">
+          <TabelaRoo />
+        </Tab>
+        <Tab tabClassName="test" eventKey="longer-tab" title="PSSS">
+          <TabelaPSSS />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
