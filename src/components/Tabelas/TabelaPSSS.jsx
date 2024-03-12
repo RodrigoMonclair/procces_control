b0f59f764e457efd8ba26b513ddf77576db06bfc
RodrigoMonclair <rmonclair@gmail.com>
2024-03-12 10:46:29 -0300

    ajuste de tabela de psss

--- src/components/Tabelas/TabelaPSSS.jsx
+++ src/components/Tabelas/TabelaPSSS.jsx
@@ -9,6 +9,7 @@ function TabelaPSSS() {
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>Servidor Licenciado</strong></th>
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>E-mail do Servidor</strong></th>
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>Início da Licença</strong></th>
+          <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>Mês do Último Recolhimento - Parte Servidor</strong></th>
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>Mês do Último Recolhimento - Parte Patronal</strong></th>
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>Mês do Último Despacho Nupac</strong></th>
           <th style={{backgroundColor: "#323234", color: "#ccc"}}><strong>PA SEI</strong></th>
@@ -21,18 +22,35 @@ function TabelaPSSS() {
           <td>Mark</td>
           <td>Otto</td>
           <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
+          <td>@mdo</td>
         </tr>
         <tr>
           <td>2</td>
           <td>Jacob</td>
           <td>Thornton</td>
           <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
+          <td>@fat</td>
         </tr>
         <tr>
           <td>3</td>
           {/* <td colSpan={3}>Larry the Bird</td> */}
-          
           <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          <td>@twitter</td>
+          
         </tr>
       </tbody>
     </Table>
